d1697a3c0127b79e7f2c89b6edf1388b81faa695
Spider <modikapil@gmail.com>
2019-06-28 14:24:57 +0100

    Update RoutingSteps.java

--- src/main/java/com/bdd/steps/RoutingSteps.java
+++ src/main/java/com/bdd/steps/RoutingSteps.java
@@ -8,5 +8,44 @@ public class RoutingSteps extends BaseSteps implements En {
     @Value("${channel.routing.endpoint}")
     private String endpoint;
 
+    private String bin;
+    private String dbid;
+    private String iam_subject;
+    private ResponseEntity responseEntity;
+
+    @Given("^a valid bin \"([^\"]*)\", dbid \"([^\"]*)\", iam_subject \"([^\"]*)\"$")
+    public void aValidBinDbidIam_subject(String bin, String dbid, String iam_subject) throws Throwable {
+        this.bin = bin;
+        this.dbid = dbid;
+        this.iam_subject = iam_subject;
+    }
+
+    @And("^a valid routing request$")
+    public void aValidRoutingRequest() throws Throwable {
+        urlToTest = channelHost + routingUrl;
+    }
+
+    @When("^routing api is called$")
+    public void routingApiIsCalled() throws Throwable {
+        final Map<String, String> headerValues = ImmutableMap.of(
+                "iam_r4p_bin", bin,
+                "iam_r4p_dbid", dbid,
+                "iam_subject", iam_subject);
+
+        responseEntity = executeGet(urlToTest, buildHeaders(headerValues), String.class, "LOGIN");
+    }
+
+    @Then("^a successful routing response is returned$")
+    public void aSuccessfulResponseIsReturned() throws Throwable {
+        Assert.assertTrue(responseEntity.getStatusCode() == HttpStatus.OK);
+    }
+
+    @And("^the response contains tier (\\d+)$")
+    public void theResponseContainsTier(int arg0) throws Throwable {
+        ResponseEntity loginResponseEntity = (ResponseEntity) PlatformRegistry.getInstance().getValue("LOGIN");
+
+        LoginResponse loginResponse = (LoginResponse) mapToResponseEntity(loginResponseEntity, LoginResponse.class).getBody();
+        Assert.assertTrue(loginResponse.getTier().getCustomerTier().equals("TIER_4"));
+    }
 
 }
